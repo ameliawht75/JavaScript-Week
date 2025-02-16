4a9777cd72802dff2afe7abfed59edb01419da05
ameliawht75 <ameliawht75@gmail.com>
2025-02-16 16:10:44 -0700

    added question 2

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@
 let ages = [3, 9, 23, 64, 2, 8, 28, 93]
 console.log("Ages:", ages);
 
-//Subtract first element from last element programmatically
+//Subtract first element from last element
 let minusAge = ages[ages.length - 1] - ages[0];
 console.log("minusAge:", minusAge); //output: 90
 
@@ -24,3 +24,20 @@ let average = totalAges / ages.length
 console.log("Average", average); //output: 30.55 repeating
 
 //Question 2
+let names = ['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob'];
+let totalLetters = 0;
+
+//Calculate average letters per name
+for (let i = 0; i < names.length; i++) {
+    totalLetters += names[i].length;
+    console.log("index:", i, "name", names[i], "totalLetters:", totalLetters)
+}
+let averageName = totalLetters / names.length;
+console.log("Average letters per name:", averageName);
+
+//Concatenate names with spaces
+let concatNames = "";
+for (let i = 0; i < names.length; i++){
+    concatNames = concatNames.concat(names[i]) + " ";
+    console.log(i, "Names concatenated", concatNames);
+}
